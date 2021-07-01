b8eba32f2ba54f26770491ac33b430ababe408d1
MauricioSamboniSarria <maurix73@gmail.com>
2021-07-01 16:32:00 -0500

    Inicio de proyecto ciclo 2 java

---
+++ src/junio29.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class junio29 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        //String ingreso = scanner.nextLine();  //lee lo que pase el usuario
+        //System.out.println(ingreso);
+        int numero = scanner.nextInt();
+        System.out.println(numero);
+    }
+}
